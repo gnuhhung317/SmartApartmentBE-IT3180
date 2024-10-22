17476468b203039b3d6e1beaa39aeb2b96679da5
Duc Hung <duchung04st@gmail.com>
2024-10-22 23:29:34 +0700

    init project

---
+++ src/main/java/com/hust/smart_apartment/SmartApartmentApplication.java
@@ -0,0 +1,13 @@
+package com.hust.smart_apartment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartApartmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartApartmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hust/smart_apartment/SmartApartmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hust.smart_apartment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartApartmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
